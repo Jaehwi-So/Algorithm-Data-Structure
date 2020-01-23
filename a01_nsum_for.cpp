3cbf5faade5001c5ef8d2d262db4051e966e766f
Jaehwi-So <57826925+Jaehwi-So@users.noreply.github.com>
2020-01-24 06:05:53 +0900

    n까지의 합

---
+++ a01_nsum_for.cpp
@@ -0,0 +1,17 @@
+﻿//#3. 1부터 n까지의 합 구하기 (반복)
+#include<stdio.h>
+int getSum(int n) {
+	int sum = 0;
+	for (int i = 1; i <= n; i++) {
+		sum += i;
+	}
+	return sum;
+}
+
+int main() {
+	int num;
+	printf("정수 n 입력 : ");
+	scanf_s("%d", &num);
+	printf("1부터 %d까지의 합 : %d", num, getSum(num));
+	return 0;
+}

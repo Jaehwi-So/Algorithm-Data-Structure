8f9534ae6c9c2fa88c26f93cbaa29f03ab92750f
Jaehwi-So <57826925+Jaehwi-So@users.noreply.github.com>
2020-01-24 06:08:37 +0900

    1부터 n까지의 합(순환)

---
+++ a01_nsum_recursive.cpp
@@ -0,0 +1,18 @@
+﻿//#4. 1부터 n까지의 합 구하기 (순환)
+#include<stdio.h>
+int getSum(int n) {
+	if (n == 1) {
+		return 1;
+	}
+	else {
+		return n + getSum(n - 1);
+	}
+}
+
+int main() {
+	int num;
+	printf("정수 n 입력 : ");
+	scanf_s("%d", &num);
+	printf("1부터 %d까지의 합 : %d", num, getSum(num));
+	return 0;
+}

3493b12f2891e57a7dff504ad8581a9936b766fe
Jaehwi-So <57826925+Jaehwi-So@users.noreply.github.com>
2020-01-24 06:05:33 +0900

    2진수 변환

---
+++ a01_decimal_convert_bin.cpp
@@ -0,0 +1,24 @@
+﻿//#7 양의 정수 n을 입력받아 2진수로 변환된 결과를 출력
+#include<stdio.h>
+
+void binary_convert(int num) {
+	int bin[64];
+	int no=0;
+	while (num > 1) {
+		bin[no++] = num % 2;
+		num = num / 2;
+	}
+	bin[no] = num;
+
+	for (int i = no; i >= 0; i--) {
+		printf("%d", bin[i]);
+	}
+
+}
+int main() {
+	unsigned int num;
+	printf("정수 n 입력 : ");
+	scanf("%d", &num);
+	binary_convert(num);
+	return 0;
+}

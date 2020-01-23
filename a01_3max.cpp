d40cf2de7cdc9a9bc4e5fa0be88ad0e520ca5cf6
Jaehwi-So <57826925+Jaehwi-So@users.noreply.github.com>
2020-01-24 06:04:49 +0900

    세 정수 중간값

---
+++ a01_3max.cpp
@@ -0,0 +1,16 @@
+﻿//#2. 세 정수를 입력받아 중간값 구하기
+#include<stdio.h>
+
+int main() {
+	int num[3];
+	printf("정수 세 개 입력 : ");
+	scanf("%d %d %d", num, num+1, num+2);
+	int max = num[0];
+	for (int i = 0; i < 3; i++) {
+		if (max < num[i]) {
+			max = num[i];
+		}
+	}
+	printf("max : %d", max);
+	return 0;
+}

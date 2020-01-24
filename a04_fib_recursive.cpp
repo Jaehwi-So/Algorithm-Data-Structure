0bd6d9e1046b18f0be31c5b33be8be16047980a3
Jaehwi-So <57826925+Jaehwi-So@users.noreply.github.com>
2020-01-24 23:43:23 +0900

    n번째 피보나치 수(순환)

---
+++ a04_fib_recursive.cpp
@@ -0,0 +1,19 @@
+﻿/*
+피보나치 수열의 n번째 수 구하기(순환)
+입력 : 정수 n, 출력 : n번째 수 X
+*/
+#include<stdio.h>
+int get_fib(int n) {
+	if (n <= 1) {
+		return n;
+	}
+	return get_fib(n - 2) + get_fib(n - 1);
+
+}
+int main() {
+	int num = 0;
+	printf("n번째 수를 입력해주세요 ");
+	scanf("%d", &num);
+	printf("피보나치 연산 결과 : %d", get_fib(num));
+	return 0;
+}

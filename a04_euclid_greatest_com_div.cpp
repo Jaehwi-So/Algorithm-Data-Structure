123f51bc77cb32e64b236d07e178c807bf3a4285
Jaehwi-So <57826925+Jaehwi-So@users.noreply.github.com>
2020-01-24 18:58:30 +0900

    유클리드 호제법 이용 최대공약수

---
+++ a04_euclid_greatest_com_div.cpp
@@ -0,0 +1,21 @@
+﻿/*
+문제 : 유클리드 호제법을 이용하여 두개의 정수 x, y에 대한 최대공약수 구하기
+입력 : 정수 X, Y / 출력 : 최대공약수 T
+*/
+#include<stdio.h>
+int get_greatest_cd(int x, int y) {
+	if (y==0) {
+		return x;
+	}
+	else {
+		get_greatest_cd(y, x % y); 
+	}
+}
+int main() {
+	int x, y, t;
+	printf("두개의 정수 입력 : ");
+	scanf("%d %d",&x ,&y);
+	t = get_greatest_cd(x, y);
+	printf("최대공약수 결과 : %d", t);
+	return 0;
+}

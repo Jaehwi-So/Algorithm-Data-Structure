2d28f65889be9f4c013c97fcf8d3f4abcd9097cf
Jaehwi-So <57826925+Jaehwi-So@users.noreply.github.com>
2020-01-24 18:58:40 +0900

    팩토리얼(반복)

---
+++ a04_factorial_iter.cpp
@@ -0,0 +1,15 @@
+﻿/*
+반복을 사용하여 정수 n의 팩토리얼 구하기
+*/
+#include<stdio.h>
+int get_factorial(int n) {
+	int sum=1;
+	for (int i = 1; i <= n; i++) {
+		sum *= i;
+	}
+	return sum;
+}
+int main() {
+	printf("팩토리얼 연산 결과 : %d", get_factorial(5));
+	return 0;
+}

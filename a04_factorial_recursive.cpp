2f6b2cdf56353028e8175951345c6c16388f0fdd
Jaehwi-So <57826925+Jaehwi-So@users.noreply.github.com>
2020-01-24 18:58:48 +0900

    팩토리얼(순환)

---
+++ a04_factorial_recursive.cpp
@@ -0,0 +1,14 @@
+﻿/*
+재귀 방식(recursive)를 사용하여 정수 n의 팩토리얼 구하기
+*/
+#include<stdio.h>
+int get_factorial(int n) {
+	if (n <= 1) {
+		return 1;
+	}
+	return get_factorial(n - 1) * n;
+}
+int main() {
+	printf("팩토리얼 연산 결과 : %d",get_factorial(5));
+	return 0;
+}

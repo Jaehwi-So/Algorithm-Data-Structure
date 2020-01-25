f017ae23f2f1f13721367c1487f9b59a70540c3b
Jaehwi-So <57826925+Jaehwi-So@users.noreply.github.com>
2020-01-26 06:08:48 +0900

    문자열에서 문자 검색

---
+++ a07_str_search_char.cpp
@@ -0,0 +1,29 @@
+﻿/*
+#문제 : 문자열 "My String is the best" 에서 M, m ,B, a 문자가 있는지 검사하기
+*/
+#include<stdio.h>
+#include<stdlib.h>
+
+int scan_str(char* s, char c) {
+	int i = 0;
+	while (s[i] != c) {
+		if (s[i] == '\0') {
+			printf("%c 검색 실패\n", c);
+			return -1;
+		}
+		i++;
+	}
+	printf("%c 검색 성공\n",c);
+	return i;
+}
+int main() {
+	char* str = "My String is the best";
+	int ck;
+	ck=scan_str(str, 'M');
+	ck=scan_str(str, 'm');
+	ck=scan_str(str, 'B');
+	ck=scan_str(str, 'a');
+
+
+	return 0;
+}

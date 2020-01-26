629aab074f29c460122f252ff621bb0a644a3023
Jaehwi-So <57826925+Jaehwi-So@users.noreply.github.com>
2020-01-26 23:33:24 +0900

    단순 연결 리스트 구현

---
+++ a08_linkedlist_singly00_main.cpp
@@ -0,0 +1,32 @@
+﻿/*
+#단순 연결 리스트 구현
+*/
+#include<stdio.h>
+#include<stdlib.h>
+#include "a08_linkedlist_singly01.h"
+int main() {
+	Node* n = NULL;
+
+	for (int i = 1; i <= 10; i++) {
+		printf("헤드에 노드 %d 삽입 : ",i);
+		n = first_insert_node(n, i);
+		print_node(n);
+	}
+
+	for (int i = 1; i <= 2; i++) {
+		printf("헤드 노드 삭제 : ");
+		n = first_delete_node(n);
+		print_node(n);
+	}
+
+	for (int i = 1; i < 10; i+=2) {
+		printf("index %d 다음 노드 삭제 : ",i);
+		n = delete_node(n, i);
+		print_node(n);
+		i += 2;
+		printf("index %d 다음에 노드 %d 추가 : ", i, i+2);
+		n = insert_node(n, i, i+1);
+		print_node(n);
+	}
+	return 0;
+}

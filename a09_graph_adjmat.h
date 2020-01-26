191d30875e86cf7a5048f256197c1ea5b132181a
Jaehwi-So <57826925+Jaehwi-So@users.noreply.github.com>
2020-01-27 06:02:35 +0900

    그래프(인접 행렬ver) BFS/DFS

---
+++ a09_graph_adjmat.h
@@ -0,0 +1,15 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+
+#define MAX_SIZE 50
+typedef struct GraphType {
+	int v;
+	int adj_mat[MAX_SIZE][MAX_SIZE];
+	int visited[MAX_SIZE];
+}GraphType;
+
+void init_graph(GraphType* g);
+void insert_vertex(GraphType* g, int vertex);
+void insert_edge(GraphType* g, int vertex1, int vertex2);
+void print_graph(GraphType* g);
+void setVisited(GraphType *g);
---
+++ a09_graph_adjmat_main.cpp
@@ -0,0 +1,38 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+#include "a09_graph_adjmat_search.h"
+
+int main() {
+	GraphType* g;
+	g = (GraphType*)malloc(sizeof(GraphType));
+
+	init_graph(g);
+
+	for (int i = 0; i < 9; i++) {
+		insert_vertex(g, i);
+	}
+
+	insert_edge(g, 0, 1);
+	insert_edge(g, 0, 8);
+	insert_edge(g, 2, 3);
+	insert_edge(g, 2, 4);
+	insert_edge(g, 2, 5);
+	insert_edge(g, 2, 8);
+	insert_edge(g, 4, 7);
+	insert_edge(g, 5, 6);
+	insert_edge(g, 6, 7);
+	insert_edge(g, 6, 8);
+
+	printf("인접 행렬 출력\n");
+	print_graph(g);
+
+	setVisited(g);
+	printf("\nDFS Start : ");
+	dfs_mat(g, 0);
+
+	setVisited(g);
+	printf("\nBFS Start : ");
+	bfs_mat(g, 0);
+
+	return 0;
+}

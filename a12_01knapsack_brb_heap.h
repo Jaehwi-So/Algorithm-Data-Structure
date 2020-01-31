7378831dcfe2a1a2d1168ca11469c008b9aaff6f
Jaehwi-So <57826925+Jaehwi-So@users.noreply.github.com>
2020-01-31 19:39:58 +0900

    01 배낭채우기 문제(01 Knapsack, 분기한정법)

---
+++ a12_01knapsack_brb_heap.h
@@ -0,0 +1,24 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#define MAX_SIZE 100
+#define item_num 4
+typedef struct TreeNode {
+	int weight;
+	int profit;
+	int i;
+	int bound;
+} TreeNode;
+typedef TreeNode element;
+//히프
+typedef struct {
+	element data[MAX_SIZE]; //레코드
+	int heap_size;	//힙 크기
+}Heap;
+
+Heap* create_heap();
+void init_heap(Heap* h);
+void insert_heap(Heap* h, element item);
+element delete_heap(Heap* h);
+
+

6fb45a5d7eee7a982bf0451d0c2b71dec15c148e
Jaehwi-So <57826925+Jaehwi-So@users.noreply.github.com>
2020-02-02 09:09:21 +0900

    TSP (외판원 문제, 분기한정법)

---
+++ a12_tsp_heap.h
@@ -0,0 +1,25 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#define MAX_SIZE 100
+
+typedef struct Node {
+	int bound;	//현재 마디의 bound값
+	int i;	//현재 마디의 수준
+	int pt[10];	//현재 마디의 경로
+}Node;
+
+typedef Node element;
+
+//히프
+typedef struct {
+	element data[MAX_SIZE]; //레코드
+	int heap_size;	//힙 크기
+}Heap;
+
+Node create_node();
+Heap* create_heap();
+void init_heap(Heap* h);
+void insert_heap(Heap* h, element item);
+element delete_heap(Heap* h);
+
